23b14193b8cc0a58411e10bca872fee2f4254a36
beomsun5 <55180881+beomsun5@users.noreply.github.com>
2022-12-24 22:26:33 +0900

    ECG Application final version
    This is the application displaying the chart of the measured electrocardiogram (심전도 in Korean) This data is measured through ECG kit.
    ECG kit is connected to the arduino UNO board. At the board, there is Bluetooth module, HC-06, which helps Bluetooth communication with other remote devices.
    We transferred measured data through the module. (We have adapted UART communication)
    
    With this basic background, implementing Login process and bringing weather information through web crawling, we have tried to enhance our app much better than before.

---
+++ ecg_final/ecg.ino
@@ -0,0 +1,106 @@
+#include <SoftwareSerial.h>
+
+int Tx = 6; //전송 보내는핀  
+int Rx = 7; //수신 받는핀
+int count = 512; //테스트 횟수
+SoftwareSerial bluetooth(Tx,Rx);
+String A;
+int tmp1, tmp2, tmp3;
+byte ecg_data[1024];
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  //Serial.print("tmp,tmp2");
+}
+
+void loop() {
+  int start = bluetooth.read();
+  if(a != -1) {
+    count = 0;
+  }
+  if(count <1024) {
+    if (bluetooth.available()) {
+      Serial.write(bluetooth.read());
+    }
+    if (Serial.available()) {
+      bluetooth.write(Serial.read());
+    }
+    ecg_data[count] = analdkogRead(0)/4;
+    count++;
+
+    delay(20);
+  }
+  else if(count == 1024){
+    bluetooth.write(ecg_data,1024);
+    count++;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int a = bluetooth.read();
+  if(a!=-1) {
+    Serial.println(a);  
+    count = 0;  
+  }
+  if(count<512) {
+    if (bluetooth.available()) {
+      Serial.write(bluetooth.read());
+    }
+    if (Serial.available()) {
+      bluetooth.write(Serial.read());
+    }
+    /*
+    for(int i=0; i<1024; i++) {
+      cyh[i] = i%256;
+    }
+    bluetooth.write(cyh,1024);
+    */
+    tmp2 = analogRead(0);
+    Serial.print("SIG : ");
+    Serial.println(tmp2);
+ 
+    tmp1 = (tmp2 & 0xff00);
+    tmp2 = (tmp2 & 0x00ff);
+
+    ecg_data[count*2] = (tmp1>>8)+1;
+    ecg_data[count*2+1] = tmp2+1;
+
+    tmp3 = analogRead(A1);
+    Serial.print("GND : ");
+    Serial.println(tmp3);
+    //Serial.print(tmp1>>8);
+    //Serial.print(" + ");
+    //Serial.println(tmp2);
+
+
+    
+    delay(20);
+    count++;
+  
+  }
+  else if(count == 512){
+    bluetooth.write(ecg_data,1024);
+    count++;
+  }
+    //
+    //bluetooth.print("567");
+    //delay(1000);
+    
+
+/*
+    if(digitalRead(10) == 1 || digitalRead(11) == 1) {
+    
+      Serial.print('!');
+    }
+    else {
+      Serial.println(analogRead(A0));   
+      delay(20);  
+    }
+  */
+}
